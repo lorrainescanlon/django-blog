38eb3bda4f168f4c90aeed54af3ee9e4fc292d12
Lorraine Scanlon <lorrainescanlon@gmail.com>
2024-12-21 21:33:37 +0000

    installed database packages, updated env.py and settings.py files with database connection details

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--9(&u@+a#4(&hm^!=gw6n&8ey6plfxi=8kzzy4$kawz3##o=!4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-lorrainescan-djangoblog-1flyl1xd6m4.ws.codeinstitute-ide.net',
     '.herokuapp.com']
@@ -74,11 +79,16 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':
+    dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
