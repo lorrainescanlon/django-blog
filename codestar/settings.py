147316c4c1d1b063722dc72f7fdd082771c887d6
Lorraine Scanlon <lorrainescanlon@gmail.com>
2025-01-08 13:31:26 +0000

    enable serving of image files

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-lorrainescan-djangoblog-1flyl1xd6m4.ws.codeinstitute-ide.net',
     '.herokuapp.com',
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -54,6 +55,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
     'blog',
     'about',
 ]
